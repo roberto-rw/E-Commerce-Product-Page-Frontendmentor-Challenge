7640ff509e5f11477779675ed051c4e185b2206e
Roberto <robertorw7@gmail.com>
2022-07-22 22:19:10 -0600

    Se agregó la función de mostrar y ocultar el carrito

--- cart.js
+++ cart.js
@@ -2,12 +2,19 @@ const buttonAddMinus = document.querySelector('.add-sub-buttons');
 const amountDisplay = document.querySelector('.amount-display')
 const cartCounterIcon = document.querySelector('.nav__cart-counter');
 const addToCartButton = document.querySelector('.add-to-cart-button');
+const cart = document.querySelector('.nav__cart');
+const profileIcon = document.querySelector('.nav__avatar');
+const cartComponent = document.querySelector('.nav__cart-component');
 
 let counter = 0;
 
 buttonAddMinus.addEventListener('click',addMinusButton);
 addToCartButton.addEventListener('click', addToCart);
+cart.addEventListener('click', showAndHideCart);
+profileIcon.addEventListener('click', showAndHideCart);
 
+
+//Aumenta o disminuye el contador segun el botón seleccionado
 function addMinusButton(e){
     if(e.target.classList.contains('add-button') || e.target.classList.contains('add-button-img')) counter++;
 
@@ -23,4 +30,10 @@ function addToCart(){
         cartCounterIcon.classList.add('nav__cart-counter-active');
         cartCounterIcon.textContent = Number(cartCounterIcon.textContent) + counter;
     }
+}
+
+function showAndHideCart(e){
+    e.preventDefault();
+    cartComponent.classList.toggle('cart-active');
+    e.stopPropagation();
 }
