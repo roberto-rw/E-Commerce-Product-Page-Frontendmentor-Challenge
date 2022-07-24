a06961a64febb68a0126f69b93f46f96acaa6e74
Roberto <robertorw7@gmail.com>
2022-07-24 15:38:21 -0600

    Se terminó la parte del carrito de compras

--- cart.js
+++ cart.js
@@ -2,16 +2,19 @@ const buttonAddMinus = document.querySelector('.add-sub-buttons');
 const amountDisplay = document.querySelector('.amount-display')
 const cartCounterIcon = document.querySelector('.nav__cart-counter');
 const addToCartButton = document.querySelector('.add-to-cart-button');
-const cart = document.querySelector('.nav__cart');
+const cartIcon = document.querySelector('.nav__cart-icon');
 const profileIcon = document.querySelector('.nav__avatar');
 const cartComponent = document.querySelector('.nav__cart-component');
+const tableBody = document.querySelector('.nav__cart-component-table-body');
+const cartEmptyMessage = document.querySelector('.nav__cart-component-table-body-cart-empty-message');
 
 let counter = 0;
 
 buttonAddMinus.addEventListener('click',addMinusButton);
 addToCartButton.addEventListener('click', addToCart);
-cart.addEventListener('click', showAndHideCart);
+cartIcon.addEventListener('click', showAndHideCart);
 profileIcon.addEventListener('click', showAndHideCart);
+cartComponent.addEventListener('click', removeArticle);
 
 
 //Aumenta o disminuye el contador segun el botón seleccionado
@@ -30,10 +33,36 @@ function addToCart(){
         cartCounterIcon.classList.add('nav__cart-counter-active');
         cartCounterIcon.textContent = Number(cartCounterIcon.textContent) + counter;
     }
+    renderCart();
+}
+
+function renderCart(){
+    let total = 125 * Number(cartCounterIcon.textContent);
+
+    let row = `<tr class="nav__cart-component-table-row">
+                    <td class="nav__cart-component-table-cell"><img src="/images/image-product-1-thumbnail.jpg" alt="" class="nav__cart-component-table-cell-img"></td>
+                    <td class="nav__cart-component-table-cell"> Fall Limited Edition Sneakers $125.00 x ${cartCounterIcon.textContent} $${total}</td>
+                    <td class="nav__cart-component-table-cell"><img src="/images/icon-delete.svg" alt="" class="nav__cart-component-table-cell-remove-icon"></td>
+                </tr>`;
+
+    //Verifica si el carrito se encuentra vacio antes de renderizarlo
+    if(Number(cartCounterIcon.textContent) < 1){
+        cartEmptyMessage.classList.add('active');
+        cartCounterIcon.classList.remove('nav__cart-counter-active');
+        tableBody.innerHTML = '';
+    }else{
+        tableBody.innerHTML = row;
+        cartEmptyMessage.classList.remove('active');
+    }
 }
 
 function showAndHideCart(e){
-    e.preventDefault();
-    cartComponent.classList.toggle('cart-active');
-    e.stopPropagation();
+    cartComponent.classList.toggle('active');
+}
+
+function removeArticle(e){
+    if(e.target.classList.contains('nav__cart-component-table-cell-remove-icon')){
+        cartCounterIcon.textContent = Number(cartCounterIcon.textContent) - 1;
+        renderCart();  
+    }  
 }
